81875cc55bf6e006a073cea80a449e1e378f282f
Yannis <yannis.barba.dev@gmail.com>
2021-09-29 22:34:59 +0200

    add function createTile and function addTile + test on index.html with two tile

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
 // function to add tiles (new wiki about an old game)
+
+function createTile(srcImg, title, text) {
+  const newTile = document.createElement('div');
+  const newImg = document.createElement('img');
+  newImg.src = srcImg;
+
+  const newTitle = document.createElement('h3');
+  newTitle.innerHTML = title;
+
+  const newText = document.createElement('p');
+  newText.innerHTML = text;
+
+  newTile.classList.add('tile');
+  newImg.classList.add('tileImg');
+  newTitle.classList.add('tileTitle');
+  newText.classList.add('tileText');
+
+  const divImg = document.createElement('div');
+  divImg.classList.add('tileDivImg');
+  divImg.appendChild(newImg);
+
+  newTile.appendChild(divImg);
+  newTile.appendChild(newTitle);
+  newTile.appendChild(newText);
+
+  return newTile;
+}
+
+function addTile(idContainer, srcImg, title, text) {
+  console.log(idContainer, srcImg, title, text);
+  const container = document.querySelector(`#${idContainer}`);
+  const newDiv = createTile(srcImg, title, text);
+  container.appendChild(newDiv);
+}
+
+addTile(
+  'containerHome',
+  'assets/brick_breaker.PNG',
+  'Brick Breaker',
+  'Texte sur le Brick Breaker'
+);
+
+addTile(
+  'containerHome',
+  'assets/snake_game.png',
+  'Snake Game',
+  'Texte sur le Snake Game'
+);
